667180eb8461bfc0a7b634bbe8250b7417aca64f
Kristen Rieger <karieger1@gmail.com>
2015-05-24 01:37:19 -0500

    almost done

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,37 @@
 var inputBox = document.getElementById("inputBox"); 
 var button = document.getElementById("button"); 
-button.addEventListener ("click", function() {
-	console.log('inputBox.value');
+	button.addEventListener ("click", function() {
+		console.log(inputBox.value);
 });
+
+var myArray = []; //create empty array
+	button.addEventListener ("click", function() { //listen for button click, when clicked run function
+		myArray.push(inputBox.value); // push the value in the inputBox into the array
+		console.log(myArray); //log array values
+});
+
+
+var input = document.getElementById("inputBox");
+var todoList = document.getElementById("savedStuff");
+
+input.addEventListener("keyup", appendMessage);
+
+function appendMessage() {
+	if(event.keyCode === 13) { //can't figure out how to make this happen on button click, too
+		
+	savedStuff.innerHTML += "<div>" + input.value + "</div>";
+	input.value = "";
+	}
+};
+
+var newStuff = [];
+var messageBox  = document.getElementById("savedStuff");
+
+function render() {
+	newStuffInput.value = ""; //get value of input
+  	messageBox.innerHTML = ""; //clear input box
+  	savedStuff.join([" ,"]) //not working?
+  	//not sure i understood the directions of step 5 very well
+}
+
+
